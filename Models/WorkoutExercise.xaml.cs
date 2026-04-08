2873fe67befd3466675adbb49a535bfd0609dff3
Artem Baranenko <artembaranenko1005@gmail.com>
2026-04-08 21:54:41 +0800

    feat: created WorkoutService core structure

---
+++ Models/WorkoutExercise.xaml.cs
@@ -0,0 +1,6 @@
+public class WorkoutExercise
+{
+    public int ExerciseOrder { get; set; }
+    public List<TemplateExercise>? Exercise { get; set; }
+    public string? Note { get; set; }
+}
---
+++ Services/WorkoutService.xaml.cs
@@ -0,0 +1,35 @@
+public class WorkoutService
+{
+    private List<WorkoutExercise> workoutExercises = new List<WorkoutExercise>();
+
+    public void CreateWorkout(WorkoutTemplate workoutTemplate)
+    {
+
+    }
+    public void StartWorkout(WorkoutExercise workoutExercise)
+    {
+
+    }
+
+    public void FinishWorkout(WorkoutSession workoutSession)
+    {
+
+    }
+
+    public void PreviousWorkouts()
+    {
+        //return workouts
+    }
+
+    public void DisplayCurentExercise(WorkoutExercise workoutExercise)
+    {
+
+    }
+
+    public List<WorkoutExercise> SwitchExercise()
+    {
+        return workoutExercises;
+    }
+
+
+}
